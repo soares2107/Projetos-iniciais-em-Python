e8fb531bcc6d5c7d86566ca382a5fc10d4157da3
João Gabriel Soares Pereira da Silva <118543920+soares2107@users.noreply.github.com>
2024-01-16 18:43:47 -0300

    Desconto.py

--- Desconto.py
+++ Desconto.py
@@ -4,3 +4,4 @@ preço = float(input('Qual é o valor do seu Produto: R$ '))
 desconto = preço - (preço * 5/100)
 print('O produto {} que custava R$ {:.2F},na promoção com desconto de 5% vai custar R$ {:.2f}.' .format(produto,preço, desconto))
 print('='*36)   
+
